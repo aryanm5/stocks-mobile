397a744fda3242f2bcdce85a88c7b897c9f21b13
aryanm5 <aryanm.email@gmail.com>
2021-11-29 11:28:39 -0800

    Added Cocoapods and installed RHLinePlot

---
+++ stocks/StockView.swift
@@ -0,0 +1,20 @@
+//
+//  StockView.swift
+//  stocks
+//
+//  Created by Aryan Mittal on 11/29/21.
+//
+
+import SwiftUI
+
+struct StockView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct StockView_Previews: PreviewProvider {
+    static var previews: some View {
+        StockView()
+    }
+}
