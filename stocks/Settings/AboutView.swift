ace45de150160a2f2621dcda4d04802c33995034
aryanm5 <aryanm.email@gmail.com>
2021-12-07 01:15:46 -0800

    Added creators to About the App view

--- stocks/Settings/AboutView.swift
+++ stocks/Settings/AboutView.swift
@@ -17,21 +17,82 @@ struct AboutView: View {
                 
                 Text("Our specialized machine learning model analyzes previous stock behavior to reliably predict future trends. The model is retrained every day to maintain accurate predictions.")
                     .padding()
-                
-                Text("Get started by adding your favorite stocks to your Watchlist.")
-                    .padding()
-                
-                Text("Aryan Mittal\nAryav Nagar\nAnshil Patel")
-                    .padding()
             }
+            
+            PersonView(person: .aryan)
+            PersonView(person: .aryav)
+            PersonView(person: .anshil)
         }
         .navigationTitle("About the App")
         .navigationBarTitleDisplayMode(.inline)
     }
 }
 
+struct PersonView: View {
+    let person: Person
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 3) {
+            HStack {
+                Image(systemName: person.iconName)
+                    .font(.system(size: 25))
+                VStack(alignment: .leading, spacing: 0) {
+                    Text(person.name)
+                        .font(.title3)
+                        .bold()
+                    Text(person.role)
+                        .font(.callout)
+                        .foregroundColor(.secondary)
+                        .bold()
+                        .lineLimit(1)
+                        .minimumScaleFactor(0.8)
+                }
+            }
+            Text(person.desc)
+        }
+        .padding()
+        .padding(.vertical, 5)
+    }
+}
+
+enum Person: Int {
+    case aryan, aryav, anshil
+    
+    var name: String {
+        switch self {
+        case .aryan: return "Aryan Mittal"
+        case .aryav: return "Aryav Nagar"
+        case .anshil: return "Anshil Patel"
+        }
+    }
+    
+    var role: String {
+        switch self {
+        case .aryan: return "App Development and Design"
+        case .aryav: return "Data Science and Machine Learning"
+        case .anshil: return "Colors and Marketing"
+        }
+    }
+    
+    var desc: String {
+        switch self {
+        case .aryan: return "In addition to building apps for his community, Aryan enjoys writing for his blog and biking with his friends."
+        case .aryav: return "Aryav is an aspiring data scientist. He also has a strong passion for photography and loves to learn about upcoming cars."
+        case .anshil: return "Anshil is a self-employed crypto miner, earning up to $2 an hour in exchange for doing all his homework on a Chromebook."
+        }
+    }
+    
+    var iconName: String {
+        switch self {
+        case .aryan: return "apps.iphone"
+        case .aryav: return "gearshape.2"
+        case .anshil: return "megaphone"
+        }
+    }
+}
+
 struct AboutView_Previews: PreviewProvider {
     static var previews: some View {
-        AboutView()
+        AboutView().preferredColorScheme(.dark)
     }
 }
